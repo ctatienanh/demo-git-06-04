1a715d764c139cfea81323936a9c92bb12a891e5
ctatienanh <102956829+ctatienanh@users.noreply.github.com>
2022-04-22 18:05:24 +0700

    quan ly sp

---
+++ quan ly san pham/qlsp.js
@@ -0,0 +1,54 @@
+
+let product = [[],[]];
+let i=0;
+let j=0;
+function add() {
+     let neww = document.getElementById("nhap").value
+     let newwanh = document.getElementById("nanh").value
+     product[i][j] = neww;
+     j++
+    product[i][j] = newwanh;
+     i++
+    j--
+    show();
+    document.getElementById("nhap").value = ""
+    console.log(product);
+}
+function show() {
+    let a = ""
+    for (i=0; i < product.length; i++)
+    {
+
+        for (j=0; j<product[i].length; j++){
+            a+= "<tr>"
+            a+=`<td> ${[i]}</td>`
+            a+=`<td> ${product[i][j]}</td>`
+            a+=`<td><img src="${product[i][j]}"></td>`
+            a+=`<td><input type="button" value="Edit" onclick="showedit(${i})" > </td>`
+            a+=`<td><input type="button" value="Delete" onclick="xoa(${i})"> </td>`
+            a+="</tr>"
+        }
+
+    }
+    document.getElementById("display").innerHTML = a;
+}
+function xoa(index)
+{
+  product.splice(index,1);
+  x--
+  show()
+}
+
+let v= -1;
+function showedit(index)
+{
+  document.getElementById("nhap").value = product[index];
+  v = index;
+}
+function saveedit()
+{
+ product[v] = document.getElementById("nhap").value
+ v=-1;
+ show()
+}
+
